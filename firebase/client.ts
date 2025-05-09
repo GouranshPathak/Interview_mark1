f8d1b8a851afd9d8ab5d19a7c3e1366d5a64ebc5
Gautam Sutar <gautamsutar.in@gmail.com>
2025-05-09 17:51:48 +0530

    Ai_Interview_Advance

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC17khqSie4uZ69U6k8RrPu0rZhAOFpn0g",
+  authDomain: "ai-interview-mock-da418.firebaseapp.com",
+  projectId: "ai-interview-mock-da418",
+  storageBucket: "ai-interview-mock-da418.firebasestorage.app",
+  messagingSenderId: "59278480366",
+  appId: "1:59278480366:web:a225d2b6313f2ae7c8f4ff",
+  measurementId: "G-PHGQWDYYZ3",
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const analytics = getAnalytics(app);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
